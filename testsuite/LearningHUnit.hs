5982e0a9a6e2452ca3c83f1d83f84b0b8e4f6ac1
Marek Dudek <marek.dudek@gmail.com>
2015-03-31 21:34:27 +0200

    Type declarations added

--- testsuite/LearningHUnit.hs
+++ testsuite/LearningHUnit.hs
@@ -2,28 +2,30 @@ module LearningHUnit where
 
 import Test.HUnit
 
-import Test.Framework --(defaultMain)
-import Test.Framework.Providers.HUnit
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit (hUnitTestToTests)
 
---addition :: Test
+
+addition :: Test
 addition = TestCase(
     assertEqual "one plus two is three" (1+2) 3
   )
 
---multiplication :: Test
+multiplication :: Test
 multiplication = TestCase(
     assertEqual "two times two is four" (2*2) 4
   )
 
---verboseTests :: Test
+verboseTests :: Test
 verboseTests = TestList [TestLabel "Addition" addition, TestLabel "Multiplication" multiplication]
 
 
---succintTests :: Test
+succintTests :: Test
 succintTests = test [
-    "Addition" ~: "1+2 = 3" ~: (1+2) ~=? 3,
+    "Addition"       ~: "1+2 = 3" ~: (1+2) ~=? 3,
     "Multiplication" ~: "2+3 = 5" ~: (2+3) ~=? 5
   ]
+
 
 
 main :: IO ()
