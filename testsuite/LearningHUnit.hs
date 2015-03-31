8e88d5c56d85f681672978243b7f7d4e418ba8b9
Marek Dudek <marek.dudek@gmail.com>
2015-03-31 21:27:28 +0200

    Got HUnit tests to run with pseudo-graphical result display

--- testsuite/LearningHUnit.hs
+++ testsuite/LearningHUnit.hs
@@ -2,13 +2,34 @@ module LearningHUnit where
 
 import Test.HUnit
 
-t1 = TestCase(
+import Test.Framework --(defaultMain)
+import Test.Framework.Providers.HUnit
+
+--addition :: Test
+addition = TestCase(
     assertEqual "one plus two is three" (1+2) 3
   )
 
-tests = TestList [TestLabel "test 1" t1]
+--multiplication :: Test
+multiplication = TestCase(
+    assertEqual "two times two is four" (2*2) 4
+  )
 
-tests2 = test [
-    "test 2" ~: "1+2 = 3" ~: (1+2) ~=? 3,
-    "test 3" ~: "2+3 = 5" ~: (2+3) ~=? 5
+--verboseTests :: Test
+verboseTests = TestList [TestLabel "Addition" addition, TestLabel "Multiplication" multiplication]
+
+
+--succintTests :: Test
+succintTests = test [
+    "Addition" ~: "1+2 = 3" ~: (1+2) ~=? 3,
+    "Multiplication" ~: "2+3 = 5" ~: (2+3) ~=? 5
+  ]
+
+
+main :: IO ()
+main = defaultMain tests
+
+tests = [ 
+    testGroup "Verbose tests" $ hUnitTestToTests verboseTests,
+    testGroup "Succint tests" $ hUnitTestToTests succintTests 
   ]
