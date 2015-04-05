08745efc084cf108424735b17860051dcb8fa66f
Marek Dudek <marek.dudek@gmail.com>
2015-04-05 15:53:05 +0200

    more operator and function examples

--- testsuite/LearningHaskell/StartingOut.hs
+++ testsuite/LearningHaskell/StartingOut.hs
@@ -9,14 +9,18 @@ main :: IO ()
 main = defaultMain [
     testGroup "Arithmetic"  $ hUnitTestToTests arithmetic,
     testGroup "Logical"     $ hUnitTestToTests logical,
-    testGroup "Comparisons" $ hUnitTestToTests comparisons
+    testGroup "Comparisons" $ hUnitTestToTests comparisons,
+    testGroup "Enumeration" $ hUnitTestToTests enumeration,
+    testGroup "Ordering"    $ hUnitTestToTests ordering 
   ]
 
 arithmetic = test [
     2 + 15       ~=?  17,
     49 * 100     ~=?  4900,
     1892 - 1472  ~=?  420,
-    5 / 2        ~=?  2.5
+    5 / 2        ~=?  2.5,
+    5            ~=?  5.0,
+    5 `div` 2    ~=?  2
   ] 
 
 logical = test [
@@ -29,5 +33,19 @@ comparisons = test [
     5 == 5  ~=?  True,
     1 == 0  ~=?  False,
     5 /= 5  ~=?  False,
-    5 /= 4  ~=?  True
+    5 /= 4  ~=?  True,
+    5 >  5  ~=?  False,
+    5 >= 4  ~=?  True,
+    6 <  6  ~=?  False,
+    6 <= 6  ~=?  True    
+  ]
+
+enumeration = test [
+    succ 8  ~=?  9,
+    pred 8  ~=?  7
+  ]
+
+ordering = test [
+    min 9 10     ~=?  9,
+    max 100 101  ~=?  101
   ]
