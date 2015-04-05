92b71b859e8cac9f83eec53610f13bbf292e5d53
Marek Dudek <marek.dudek@gmail.com>
2015-04-05 15:27:38 +0200

    Comparison operators tested

--- testsuite/LearningHaskell/StartingOut.hs
+++ testsuite/LearningHaskell/StartingOut.hs
@@ -7,8 +7,9 @@ import Test.Framework.Providers.HUnit (hUnitTestToTests)
 
 main :: IO ()
 main = defaultMain [
-    testGroup "Arithmetic" $ hUnitTestToTests arithmetic,
-    testGroup "Logical"    $ hUnitTestToTests logical
+    testGroup "Arithmetic"  $ hUnitTestToTests arithmetic,
+    testGroup "Logical"     $ hUnitTestToTests logical,
+    testGroup "Comparisons" $ hUnitTestToTests comparisons
   ]
 
 arithmetic = test [
@@ -22,4 +23,11 @@ logical = test [
     True && False  ~=?  False,
     True || False  ~=?  True,
     not False      ~=?  True
+  ]
+
+comparisons = test [
+    5 == 5  ~=?  True,
+    1 == 0  ~=?  False,
+    5 /= 5  ~=?  False,
+    5 /= 4  ~=?  True
   ]
