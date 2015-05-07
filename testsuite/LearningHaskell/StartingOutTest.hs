ba48355e7ba29c8a2a8ae99a40f8f36fcab5212c
Marek Dudek <marek.dudek@gmail.com>
2015-05-07 22:26:49 +0200

    Sections for subchapters

--- testsuite/LearningHaskell/StartingOutTest.hs
+++ testsuite/LearningHaskell/StartingOutTest.hs
@@ -1,25 +1,30 @@
 module Main where
 
+
 import Test.HUnit
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 
 import LearningHaskell.StartingOut
 
+
 main :: IO ()
 main = defaultMain [
     testGroup "Numbers"     $ hUnitTestToTests numbers,
     testGroup "Arithmetic"  $ hUnitTestToTests arithmetic,
     testGroup "Logical"     $ hUnitTestToTests logical,
-    testGroup "Relational"  $ hUnitTestToTests relational,
+    testGroup "Equality"    $ hUnitTestToTests equality,
+    testGroup "Comparison"  $ hUnitTestToTests comparison,
+    testGroup "Conditional" $ hUnitTestToTests conditional, 
     testGroup "Enumeration" $ hUnitTestToTests enumeration,
     testGroup "Ordering"    $ hUnitTestToTests ordering, 
-    testGroup "Conditional" $ hUnitTestToTests conditional, 
     testGroup "Functions"   $ hUnitTestToTests functions 
   ]
 
+-- Ready, set, go!
+
 numbers = test [
-    5            ~=?  5.0
+    5  ~=?  5.0
   ]
 
 arithmetic = test [
@@ -36,15 +41,22 @@ logical = test [
     not False      ~=?  True
   ]
 
-relational = test [
+equality = test [
     5 == 5  ~=?  True,
     1 == 0  ~=?  False,
     5 /= 5  ~=?  False,
-    5 /= 4  ~=?  True,
+    5 /= 4  ~=?  True
+  ]
+
+comparison = test [
     5 >  5  ~=?  False,
     5 >= 4  ~=?  True,
     6 <  6  ~=?  False,
     6 <= 6  ~=?  True    
+  ]
+
+conditional = test [
+    (if True then 2 else 3)  ~=?  2
   ]
 
 enumeration = test [
@@ -57,11 +69,11 @@ ordering = test [
     max 100 101  ~=?  101
   ]
 
-conditional = test [
-    (if True then 2 else 3)  ~=?  2
-  ]
+-- Baby's first functions
 
 functions = test [
     doubleMe 3    ~=?  6,
     doubleUs 2 3  ~=?  10
   ]
+
+-- An intro to lists
